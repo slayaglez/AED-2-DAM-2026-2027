7ab04c3629278dba5541ae1abc7b8f7a0a825220
slayaglez <slayaglez@gmail.com>
2026-09-24 04:42:02 +0100

    CsvService de ficheros hecho

--- 00-repaso-java/src/main/java/com/docencia/ficheros/CsvServiceImpl.java
+++ 00-repaso-java/src/main/java/com/docencia/ficheros/CsvServiceImpl.java
@@ -1,49 +1,88 @@
 package com.docencia.ficheros;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CsvServiceImpl implements CsvService {
 
     @Override
     public List<String> leerLineasCsv(Path ruta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'leerLineasCsv'");
+        List<String> lista = new ArrayList<>();
+
+        if (!Files.exists(ruta)) throw new  IllegalArgumentException();
+
+        try (BufferedReader reader = Files.newBufferedReader(ruta, StandardCharsets.UTF_8)) {
+            String linea;
+            while((linea = reader.readLine()) != null) {
+                lista.add(linea);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(lista);
+        return lista;
     }
 
     @Override
     public List<String[]> leerRegistrosCsv(Path ruta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'leerRegistrosCsv'");
+        List<String[]> resultado = new ArrayList<>();
+        List<String> lista = new ArrayList<>(leerLineasCsv(ruta));
+        for (String linea : lista) {
+            String[] registro = linea.split(",");
+            resultado.add(registro);
+        }
+        return resultado;
     }
 
     @Override
     public void escribirLineasCsv(Path ruta, List<String> lineas) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'escribirLineasCsv'");
+        try (BufferedWriter writer = Files.newBufferedWriter(ruta, StandardCharsets.UTF_8)) {
+            for (String linea : lineas) {
+                writer.write(linea);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Integer contarRegistrosCsv(Path ruta) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contarRegistrosCsv'");
+        return leerLineasCsv(ruta).size();
     }
 
     @Override
     public List<String[]> filtrarRegistrosPorValor(Path ruta, Integer columna, String valor) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'filtrarRegistrosPorValor'");
+        List<String[]> registros = new ArrayList<>(leerRegistrosCsv(ruta));
+        List<String[]> resultado = new ArrayList<>();
+        for (String[] registro : registros) {
+            if (registro.length-1 < columna) {
+                throw new IllegalArgumentException();
+            }
+            if (registro[columna].equals(valor)) {
+                resultado.add(registro);
+            }
+        }
+        return resultado;
     }
 
     @Override
     public Map<String, Integer> contarFrecuenciaColumna(Path ruta, Integer columna) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'contarFrecuenciaColumna'");
+        Map<String, Integer> resultado = new HashMap<>();
+        List<String[]> registros = leerRegistrosCsv(ruta);
+        for (String[] registro : registros) {
+            if(!resultado.containsKey(registro[columna])){
+                resultado.put(registro[columna], 1);
+            } else {
+                resultado.put(registro[columna], resultado.get(registro[columna]) + 1);
+            }
+        }
+        return resultado;
     }
     
 }
